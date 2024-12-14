7e778707771708e7f987ae4241b3bd465ef41e11
Rony7736 <ronychakrabarty99@gmail.com>
2024-12-14 21:57:35 +0600

    added get job id dynamic, post job application, get job application and added for loop function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -28,6 +28,55 @@ async function run() {
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        // jobs related API
+        const jobsCollection = client.db('jobPortal').collection('jobs')
+        const jobApplicationCollection = client.db('jobPortal').collection('job_applications')
+
+        app.get('/jobs', async (req, res) => {
+            const cursor = jobsCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/jobs/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await jobsCollection.findOne(query)
+            res.send(result)
+        })
+
+        // job application API
+        // get all data, get one data, get some data [0, 1, many]
+
+        app.post("/job-applications", async (req, res) => {
+            const application = req.body
+            const result = await jobApplicationCollection.insertOne(application)
+            res.send(result)
+        })
+
+        app.get("/job-applications", async (req, res) => {
+            const email = req.query.email
+            const query = { applicant_email: email }
+            const result = await jobApplicationCollection.find(query).toArray()
+
+            // not the best way
+            for (const application of result) {
+                console.log(application.job_id)
+                const query1 = { _id: new ObjectId(application.job_id) }
+                const job = await jobsCollection.findOne(query1)
+                if(job){
+                    application.title = job.title
+                    application.location = job.location
+                    application.company = job.company
+                    application.company_logo = job.company_logo
+                }
+            }
+            res.send(result)
+        })
+
+
+
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
